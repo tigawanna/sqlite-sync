107fb96e3ff27e81ca55592a338208ae5bd3e457
Gioele Cantoni <gioele.cantoni@gmail.com>
2025-08-06 09:40:20 +0200

    bump version to 0.8.21

--- src/cloudsync.h
+++ src/cloudsync.h
@@ -16,7 +16,7 @@
 #include "sqlite3.h"
 #endif
 
-#define CLOUDSYNC_VERSION                       "0.8.20"
+#define CLOUDSYNC_VERSION                       "0.8.21"
 
 int sqlite3_cloudsync_init (sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
 
