6ab360dc0cd7f3efeed6bff134b2fd9bbda57b5e
Marco Bambini <marco@creolabs.com>
2025-09-27 14:08:04 +0200

    Raised version number

--- src/cloudsync.h
+++ src/cloudsync.h
@@ -20,7 +20,7 @@
 extern "C" {
 #endif
 
-#define CLOUDSYNC_VERSION                       "0.8.36"
+#define CLOUDSYNC_VERSION                       "0.8.37"
 
 int sqlite3_cloudsync_init (sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
 
